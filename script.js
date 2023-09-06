d902a551bb65485c0e77ceb8cb67732441387b25
Maha Abdelnabi <mahaomar@gmail.com>
2023-09-05 22:41:32 -0400

    get the definition

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 inputEl = document.getElementById("input");
 
+
+async function fetchData(word){
+const searchWord = word
+
+//fetch api
+const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
+console.log(apiUrl);
+ const result = await fetch(apiUrl).then((res) => res.json());
+ console.log(result[0].meanings[0].definitions[0].definition);
+ 
+
+    
+     
+     };
+
+ 
 //get the value of the input
 inputEl.addEventListener("keyup",(e)=>{
-    console.log(e.target.value)
+    //only the enter btn for search
+    if(e.target.value && e.key === "Enter"){
+        fetchData(e.target.value);
+    }
 })
